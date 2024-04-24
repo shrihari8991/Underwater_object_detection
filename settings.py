c564c845f533646ba41d755df62c2db3f22182f0
shrihari8991 <78495265+shrihari8991@users.noreply.github.com>
2024-04-25 00:04:17 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -30,13 +30,13 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'detected_img.png'
 
 #Videos config
 VIDEO_DIR = ROOT / 'videos'
-VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
-VIDEO_2_PATH = VIDEO_DIR / 'video_2.mp4'
-VIDEO_3_PATH = VIDEO_DIR / 'video_3.mp4'
+VIDEO_1_PATH = VIDEO_DIR / 'videoplayback (1).mp4'
+#VIDEO_2_PATH = VIDEO_DIR / 'video_2.mp4'
+#VIDEO_3_PATH = VIDEO_DIR / 'video_3.mp4'
 VIDEOS_DICT = {
    'video_1': VIDEO_1_PATH,
-   'video_2': VIDEO_2_PATH,
-  'video_3': VIDEO_3_PATH,
+   #'video_2': VIDEO_2_PATH,
+  #'video_3': VIDEO_3_PATH,
 }
 
 # ML Model config
