d074cfd84db640c5ffcb727e62fd594b58377dff
shrihari8991 <78495265+shrihari8991@users.noreply.github.com>
2024-04-24 21:07:06 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Underwater_object_detection
-This contains streamlit app for underwater object detection and annotation toolkit.
+# Object-Detection-Annotation-Tool
+Object Detection and Annotation Tool is designed to identify objects within an image. It not only detects existing objects but also provides a user-friendly interface to annotate any undetected objects. The tool allows users to save annotated images in both YOLO and PASCAL VOC formats.
+
+ ## Demo
+ https://github.com/PratikRMarabille/Object-Detection-Annotation-Tool/assets/155374707/0ad8e503-359c-47ff-a892-b5bac27a4fe5
+
+ ## Installation
+
+ Clone the repository: git clone "https://github.com/PratikRMarabille/Object-Detection-Annotation-Tool.git" <br/>
+ Change to the repository directory: ```cd Object-Detection-Annotation-Tool ``` <br/>
+ Create a virtual environment: ```python -m venv env_name```
+
+ ## Requirements
+```bash
+    pip install ultralytics
+    pip install -r requirements
+```
+## Usage
+
+* Execute the application using the command: ```streamlit run app.py``` 
+* A new browser window will open, displaying the app.
+* The main page showcases an initial image alongside its corresponding object-detected version. 
+* Upload an image by clicking the "Browse files" button.
+* If some objects are not detected then use the Object annotation tool.
+* Select the image, start to add bounding box by clicking on the Add bounding box then name the object using the drop down menu.
+* Then click on the save button.
+* The annotated image will get saved in the YOLOv8 as well as PASCAL VOC format.
+* The YOLOv8 formate will get saved in the labels directory and PASCAL VOC format will get saved in the img_dir.
---
+++ app.py
@@ -0,0 +1,335 @@
+# Python In-built packages
+from pathlib import Path
+import PIL
+import streamlit.components.v1 as com
+import streamlit as st
+import os
+from streamlit_img_label import st_img_label
+from streamlit_img_label.manage import ImageManager, ImageDirManager
+import xml.etree.ElementTree as ET
+import glob
+import os
+import json
+
+# External packages
+import streamlit as st
+
+# Local Modules
+import settings
+import helper
+
+# Setting page layout
+st.set_page_config(
+    page_title="Object Detection",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# Main page heading
+st.title("Object Detection")
+
+# Sidebar
+st.sidebar.header("ML Model Config")
+
+#st.markdown("<div style='background-color:#007bff; padding:10px; color:#fff; font-size:1.5em; text-align:center; border-bottom:1px solid #fff; margin-bottom:20px;'>ML Model Config</div>", unsafe_allow_html=True)
+# Model Options
+model_type = st.sidebar.radio(
+    "Select Task", ['Detection'])
+
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 25, 100, 40)) / 100
+
+# Selecting Detection Or Segmentation
+if model_type == 'Detection':
+    model_path = Path(settings.DETECTION_MODEL)
+# elif model_type == 'Segmentation':
+#     model_path = Path(settings.SEGMENTATION_MODEL)
+
+# Load Pre-trained ML Model
+try:
+    model = helper.load_model(model_path)
+except Exception as ex:
+    st.error(f"Unable to load model. Check the specified path: {model_path}")
+    st.error(ex)
+
+st.sidebar.header("Image/Video Config")
+source_radio = st.sidebar.radio(
+    "Select Source", settings.SOURCES_LIST)
+
+source_img = None
+# If image is selected
+if source_radio == settings.IMAGE:
+    source_img = st.sidebar.file_uploader(
+        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        try:
+            if source_img is None:
+                default_image_path = str(settings.DEFAULT_IMAGE)
+                default_image = PIL.Image.open(default_image_path)
+                st.image(default_image_path, caption="Default Image",
+                         use_column_width=True)
+            else:
+                uploaded_image = PIL.Image.open(source_img)
+                st.image(source_img, caption="Uploaded Image",
+                         use_column_width=True)
+        except Exception as ex:
+            st.error("Error occurred while opening the image.")
+            st.error(ex)
+
+    with col2:
+        if source_img is None:
+            default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
+            default_detected_image = PIL.Image.open(
+                default_detected_image_path)
+            st.image(default_detected_image_path, caption='Detected Image',
+                     use_column_width=True)
+        else:
+            if st.sidebar.button('Detect Objects'):
+                res = model.predict(uploaded_image,
+                                    conf=confidence
+                                    )
+                boxes = res[0].boxes
+                res_plotted = res[0].plot()[:, :, ::-1]
+                st.image(res_plotted, caption='Detected Image',
+                         use_column_width=True)
+                try:
+                    with st.expander("Detection Results"):
+                        for box in boxes:
+                            st.write(box.data)
+                except Exception as ex:
+                    # st.write(ex)
+                    st.write("No image is uploaded yet!")
+
+
+# elif source_radio == settings.VIDEO:
+#     helper.play_stored_video(confidence, model)
+
+# elif source_radio == settings.WEBCAM:
+#     helper.play_webcam(confidence, model)
+
+# elif source_radio == settings.RTSP:
+#     helper.play_rtsp_stream(confidence, model)
+
+# elif source_radio == settings.YOUTUBE:
+#     helper.play_youtube_video(confidence, model)
+
+else:
+    st.error("Please select a valid source type!")
+
+st.sidebar.header("Detection Tool")
+
+st.markdown("<h1>Object Annotation Tool</h1>", unsafe_allow_html=True)
+st.markdown("<div class='container'>", unsafe_allow_html=True)
+
+
+def run(img_dir, labels):
+    st.set_option("deprecation.showfileUploaderEncoding", False)
+    idm = ImageDirManager(img_dir)
+
+    if "files" not in st.session_state:
+        st.session_state["files"] = idm.get_all_files()
+        st.session_state["annotation_files"] = idm.get_exist_annotation_files()
+        st.session_state["image_index"] = 0
+    else:
+        idm.set_all_files(st.session_state["files"])
+        idm.set_annotation_files(st.session_state["annotation_files"])
+    
+    def refresh():
+        st.session_state["files"] = idm.get_all_files()
+        st.session_state["annotation_files"] = idm.get_exist_annotation_files()
+        st.session_state["image_index"] = 0
+
+    def next_image():
+        image_index = st.session_state["image_index"]
+        if image_index < len(st.session_state["files"]) - 1:
+            st.session_state["image_index"] += 1
+        else:
+            st.warning('This is the last image.')
+
+    def previous_image():
+        image_index = st.session_state["image_index"]
+        if image_index > 0:
+            st.session_state["image_index"] -= 1
+        else:
+            st.warning('This is the first image.')
+
+    def next_annotate_file():
+        image_index = st.session_state["image_index"]
+        next_image_index = idm.get_next_annotation_image(image_index)
+        if next_image_index:
+            st.session_state["image_index"] = idm.get_next_annotation_image(image_index)
+        else:
+            st.warning("All images are annotated.")
+            next_image()
+
+    def go_to_image():
+        file_index = st.session_state["files"].index(st.session_state["file"])
+        st.session_state["image_index"] = file_index
+
+    # Sidebar: show status
+    n_files = len(st.session_state["files"])
+    n_annotate_files = len(st.session_state["annotation_files"])
+    st.sidebar.write("Total files:", n_files)
+    st.sidebar.write("Total annotate files:", n_annotate_files)
+    st.sidebar.write("Remaining files:", n_files - n_annotate_files)
+
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        # Save the uploaded image to the img_dir
+        with open(os.path.join(img_dir, uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        # Refresh the session state to include the new file
+        refresh()
+
+    st.sidebar.selectbox(
+        "Files",
+        st.session_state["files"],
+        index=st.session_state["image_index"],
+        on_change=go_to_image,
+        key="file",
+    )
+    col1, col2 = st.sidebar.columns(2)
+    with col1:
+        st.button(label="Previous image", on_click=previous_image)
+    with col2:
+        st.button(label="Next image", on_click=next_image)
+    st.sidebar.button(label="Next need annotate", on_click=next_annotate_file)
+    st.sidebar.button(label="Refresh", on_click=refresh)
+
+    # Main content: annotate images
+    img_file_name = idm.get_image(st.session_state["image_index"])
+    img_path = os.path.join(img_dir, img_file_name)
+    im = ImageManager(img_path)
+    img = im.get_img()
+    resized_img = im.resizing_img()
+    resized_rects = im.get_resized_rects()
+    rects = st_img_label(resized_img, box_color="red", rects=resized_rects)
+
+    def annotate():
+        im.save_annotation()
+        image_annotate_file_name = img_file_name.split(".")[0] + ".xml"
+        if image_annotate_file_name not in st.session_state["annotation_files"]:
+            st.session_state["annotation_files"].append(image_annotate_file_name)
+        next_annotate_file()
+
+        # Save annotated information to a text file
+        save_annotation_to_text(rects)
+
+    def save_annotation_to_text(rects):
+        annotation_text = ""
+        for i, rect in enumerate(rects):
+            label = rect.get('label', 'Unknown')
+            x, y, width, height = rect.get('left', 0), rect.get('top', 0), rect.get('width', 0), rect.get('height', 0)
+            annotation_text += f"{label} {x} {y} {width} {height}\n"
+
+        annotation_file_name = img_file_name.split(".")[0] + ".txt"
+        annotation_file_path = os.path.join(img_dir, annotation_file_name)
+
+        if annotation_text.strip():  # Check if there's any valid annotation before writing the file
+            with open(annotation_file_path, "w") as annotation_file:
+                annotation_file.write(annotation_text)
+
+            st.success(f"Text file saved at: {annotation_file_path}")  # Display a success message
+            return annotation_file_path  # Return the path for download
+        else:
+            st.warning("No valid annotations to save.")
+            return None
+
+
+    if rects:
+        st.button(label="Save", on_click=annotate)
+        preview_imgs = im.init_annotation(rects)
+
+        for i, prev_img in enumerate(preview_imgs):
+            prev_img[0].thumbnail((200, 200))
+            col1, col2 = st.columns(2)
+            with col1:
+                col1.image(prev_img[0])
+            with col2:
+                default_index = 0
+                if prev_img[1]:
+                    default_index = labels.index(prev_img[1])
+
+                select_label = col2.selectbox(
+                    "Label", labels, key=f"label_{i}", index=default_index
+                )
+                im.set_annotation(i, select_label)
+
+if __name__ == "__main__":
+    custom_labels = ["Holothurian", "Echinus", "Scallop","Starfish","Crab","Fish","JellyFish","Shrimp","Small Fish","Other Bio","Non Plastic","Plastic","ROV","Timestamp","Artifacts","Divers"]
+    run("img_dir", custom_labels)
+
+#convert pascal voc to yolo format
+
+def xml_to_yolo_bbox(bbox, w, h):
+    # xmin, ymin, xmax, ymax
+    x_center = ((bbox[2] + bbox[0]) / 2) / w
+    y_center = ((bbox[3] + bbox[1]) / 2) / h
+    width = (bbox[2] - bbox[0]) / w
+    height = (bbox[3] - bbox[1]) / h
+    return [x_center, y_center, width, height]
+
+
+def yolo_to_xml_bbox(bbox, w, h):
+    # x_center, y_center width heigth
+    w_half_len = (bbox[2] * w) / 2
+    h_half_len = (bbox[3] * h) / 2
+    xmin = int((bbox[0] * w) - w_half_len)
+    ymin = int((bbox[1] * h) - h_half_len)
+    xmax = int((bbox[0] * w) + w_half_len)
+    ymax = int((bbox[1] * h) + h_half_len)
+    return [xmin, ymin, xmax, ymax]
+
+
+classes = []
+input_dir = "img_dir/"
+output_dir = "labels/"
+image_dir = "img_dir/"
+
+# create the labels folder (output directory)
+#os.mkdir(output_dir)
+
+# identify all the xml files in the annotations folder (input directory)
+files = glob.glob(os.path.join(input_dir, '*.xml'))
+# loop through each 
+for fil in files:
+    basename = os.path.basename(fil)
+    filename = os.path.splitext(basename)[0]
+    # check if the label contains the corresponding image file
+    supported_extensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp']
+    if not any(os.path.exists(os.path.join(image_dir, f"{filename}.{ext}")) for ext in supported_extensions):
+        print(f"{filename} image does not exist!")
+        continue
+
+    result = []
+
+    # parse the content of the xml file
+    tree = ET.parse(fil)
+    root = tree.getroot()
+    width = int(root.find("size").find("width").text)
+    height = int(root.find("size").find("height").text)
+
+    for obj in root.findall('object'):
+        label = obj.find("name").text
+        # check for new classes and append to list
+        if label not in classes:
+            classes.append(label)
+        index = classes.index(label)
+        pil_bbox = [int(x.text) for x in obj.find("bndbox")]
+        yolo_bbox = xml_to_yolo_bbox(pil_bbox, width, height)
+        # convert data to string
+        bbox_string = " ".join([str(x) for x in yolo_bbox])
+        result.append(f"{index} {bbox_string}")
+
+    if result:
+        # generate a YOLO format text file for each xml file
+        with open(os.path.join(output_dir, f"{filename}.txt"), "w", encoding="utf-8") as f:
+            f.write("\n".join(result))
+
+# generate the classes file as reference
+with open('classes.txt', 'w', encoding='utf8') as f:
+    f.write(json.dumps(classes))
